05d801105e3b643e7537f4a8a04c24b2d66da693
origin <tnk@nerrog.net>
2020-08-16 16:46:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Easy ADBuilder/Main.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Easy_ADBuilder
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trop = new MetroFramework.Controls.MetroTextBox();
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // trop
+            // 
+            // 
+            // 
+            // 
+            this.trop.CustomButton.Image = null;
+            this.trop.CustomButton.Location = new System.Drawing.Point(470, 2);
+            this.trop.CustomButton.Name = "";
+            this.trop.CustomButton.Size = new System.Drawing.Size(297, 297);
+            this.trop.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.trop.CustomButton.TabIndex = 1;
+            this.trop.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.trop.CustomButton.UseSelectable = true;
+            this.trop.CustomButton.Visible = false;
+            this.trop.Lines = new string[] {
+        "Trop"};
+            this.trop.Location = new System.Drawing.Point(23, 63);
+            this.trop.MaxLength = 32767;
+            this.trop.Multiline = true;
+            this.trop.Name = "trop";
+            this.trop.PasswordChar = '\0';
+            this.trop.ReadOnly = true;
+            this.trop.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.trop.SelectedText = "";
+            this.trop.SelectionLength = 0;
+            this.trop.SelectionStart = 0;
+            this.trop.ShortcutsEnabled = true;
+            this.trop.Size = new System.Drawing.Size(770, 302);
+            this.trop.TabIndex = 0;
+            this.trop.Text = "Trop";
+            this.trop.UseSelectable = true;
+            this.trop.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.trop.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(23, 380);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(111, 15);
+            this.metroCheckBox1.TabIndex = 1;
+            this.metroCheckBox1.Text = "利用規約に同意";
+            this.metroCheckBox1.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Enabled = false;
+            this.metroButton1.Location = new System.Drawing.Point(439, 380);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(354, 47);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "ADB環境構築開始";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroCheckBox1);
+            this.Controls.Add(this.trop);
+            this.Name = "Main";
+            this.Text = "Easy ADBuilder";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox trop;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
+
---
+++ Easy ADBuilder/Main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+
+namespace Easy_ADBuilder
+{
+    public partial class Main : MetroForm
+    {
+        Thread thread;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void ChkBox()
+        {
+            //チェックボックス監視
+            while (true)
+            {
+                if (metroCheckBox1.Checked == true)
+                {
+                    //metroButton1.Enabled = true;
+                    this.Invoke((MethodInvoker)(() => metroButton1.Enabled = true));
+                }
+
+                if (metroCheckBox1.Checked == false)
+                {
+                    this.Invoke((MethodInvoker)(() => metroButton1.Enabled = false));
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Task task = Task.Run(() => {
+                ChkBox();
+            });
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            //メイン処理開始
+            thread.Abort();
+            thread.Join();
+            string path = @"C:\adb";
+            if (Directory.Exists(path))
+            {
+                DialogResult result = MessageBox.Show("既にCドライブにadbフォルダが存在します\r\nこのまま続行しますか？",
+                "質問",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Exclamation,
+                MessageBoxDefaultButton.Button2);
+
+                if (result == DialogResult.Yes)
+                {
+
+                }
+                else if (result == DialogResult.No)
+                {
+                    MessageBox.Show("いいえが選択されました\r\nプログラムを終了します");
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+            }
+            //コピー処理
+            string sourcePath = $@"{(System.Environment.CurrentDirectory)}\platform-tools";
+            string destinationPath = @"C:\adb";
+
+        }
+    }
+}
